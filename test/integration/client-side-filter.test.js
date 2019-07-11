d59c0f12d8306b487a3f5588fdc4353d3a28be9c
Bruce He <heganjie@gmail.com>
2019-07-11 14:02:29 +0800

    fix(client-side-test): add bulk create test

--- test/integration/client-side-filter.test.js
+++ test/integration/client-side-filter.test.js
@@ -3,12 +3,17 @@
 const chai = require('chai'),
   expect = chai.expect,
   Support = require('../unit/support'),
-  Sequelize = Support.Sequelize;
+  Sequelize = Support.Sequelize,
+  _ = require('lodash');
 
 describe('Client side filter', () => {
   let sequelize, DB2Test;
   before(() => {
-    sequelize = Support.createSequelizeInstance({ logging: console.log });
+    // 兼容旧版本调用方式
+    sequelize = Support.createSequelizeInstance({
+      logging: console.log,
+      operatorsAliases: _.mapKeys(Sequelize.Op, (v, k) => `$${k}`)
+    });
     DB2Test = sequelize.define('DB2Test', {
       id: {
         type: Sequelize.STRING(32),
@@ -50,7 +55,40 @@ describe('Client side filter', () => {
     });
   });
 
-  /*it('should be find success(findOne)', () => {
+  it('should be bulk create success', () => {
+    const s2 = {
+      id: 's2',
+      name: 'slice_02',
+      visitCount: 20,
+      price: 10.4,
+      is_private: false,
+      params: { type: 'bar' }
+    };
+    const s3 = {
+      id: 's3',
+      name: 'slice_03',
+      visitCount: 30,
+      price: 15.8,
+      is_private: true,
+      params: { type: 'pie' }
+    };
+    return DB2Test.bulkCreate([s2, s3])
+      .then(() => {
+        return DB2Test.findAll({
+          where: {
+            id: { $in: ['s2', 's3'] }
+          },
+          raw: true
+        });
+      })
+      .then(slices => {
+        const keys = Object.keys(s2);
+        const picked = slices.map(s => _.pick(s, keys));
+        expect(picked).to.deep.equal([s2, s3]);
+      });
+  });
+
+  it('should be find success(findOne)', () => {
     return DB2Test.findOne({
       where: {
         id: 's1'
@@ -65,9 +103,9 @@ describe('Client side filter', () => {
       expect(is_private).to.equal(true);
       expect(params && params.type).to.equal('line');
     });
-  });*/
+  });
 
-  /*it('should be find success(findAll)', () => {
+  it('should be find success(findAll)', () => {
     return DB2Test.findAll({
       where: {
         id: 's1'
@@ -75,23 +113,6 @@ describe('Client side filter', () => {
       raw: true
     }).then(arr => {
       const { id, name, visitCount, price, is_private, params } = arr[0] || { };
-      expect(id).to.equal('s1');
-      expect(name).to.equal('slice_01');
-      expect(visitCount).to.equal(10);
-      expect(price).to.equal(5.2);
-      expect(is_private).to.equal(true);
-      expect(params && params.type).to.equal('line');
-    });
-  });*/
-
-  it('should be find success', () => {
-    return DB2Test.findOne({
-      where: {
-        id: 's1'
-      },
-      raw: true
-    }).then(slice1 => {
-      const { id, name, visitCount, price, is_private, params } = slice1 || { };
       expect(id).to.equal('s1');
       expect(name).to.equal('slice_01');
       expect(visitCount).to.equal(10);
